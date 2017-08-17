b25ff64cdca1c1bea42490e4e401dc6eba637f1b
Evelyn Fenner-Dorrity and Mitch Long <mitch@>
2017-08-17 15:46:45 -0700

    adds branching to business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,57 @@
 // Business Logic
 var convert = function (inputNumber){
-  var symbol = ["I", "V", "X", "L", "C", "D", "M"];
-  var value = [1, 5,10,50,100,500,1000];
+  var symbol = ["M","D", "C", "L","X","V","I"];
+  var value = [1000,500,100,50,10,5,1];
   var numerals=[];
   var bookmark = [];
   var sum = 0
+  var input =
 
 
-  for(var i =0; i < value.length; i++){
-
-    if(value[i] <= inputNumber ){
+    if(input>=1000){
+      input = inputNumber
+      numerals.push("M") ;
+    }else if (input>=500){
+      numerals.push("D");
+      return ;
+    }else if (input>=100){
+      numerals.push("C");
+      return ;
+    }else if (input>=50){
+      numerals.push("L")
+      return ;
+    }else if (input>=10){
+      numerals.push("X")
+      return ;
+    }else if (input>=5){
+      numerals.push("V")
+      return ;
+    }else{
+      numerals.push("I")
+      return ;
     }
-    else{
-      numerals.push(symbol[i-1]);
-      bookmark.push(i);
 
-      console.log(bookmark);
-      console.log(numerals);
-      // return bookmark, numerals;
-      // return bookmark.toString;
-
-      return symbol[i-1];
-
-
-    };
-  };
 };
+
+  // for(var i =0; i < value.length; i++){
+  //
+  //   if(value[i] <= inputNumber ){
+  //   }
+  //   else{
+  //     numerals.push(symbol[i-1]);
+  //     bookmark.push(i);
+  //
+  //     console.log(bookmark);
+  //     console.log(numerals);
+  //     // return bookmark, numerals;
+  //     // return bookmark.toString;
+  //
+  //     return symbol[i-1];
+  //
+  //
+  //   };
+  // };
+
 
 
 
